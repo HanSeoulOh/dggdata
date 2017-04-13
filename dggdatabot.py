268beb2969333d1bae2bc1cf94b139165b87ee10
BOOTCAMP\Han Du <du.yuehan@gmail.com>
2017-04-13 00:49:47 -0400

    last python

--- dggdatabot.py
+++ dggdatabot.py
@@ -8,7 +8,16 @@ class dggDataBot(scrapy.Spider):
     ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        for month in response.css('a::attr(href)').extract():
+        	if month[:21] == "/Destinygg%20chatlog/":
+        		next_url = response.urljoin(month)
+        		yield scrapy.Request(next_url, callback = self.parseUsers)
+
+    def parseUsers(self, response):
+    	for user in response.css('a::attr(href)').extract():
+    		if user.split('/')[2] == 'userlogs':
+    			next_url = reponse.urljoin(user)
+    			yield scrapy.Request(next_url, callback = self.parseUser)
+
+   	def parseUser(self, reponse):
+   		
